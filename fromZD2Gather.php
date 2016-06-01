101987beefd71623837339fe926a71528340b19e
amaendeepm <amandeepm@gmx.com>
2016-06-01 12:57:11 +0200

    Create fromZD2Gather.php

---
+++ fromZD2Gather.php
@@ -0,0 +1,182 @@
+<?php
+
+// Sequence is : English, DK, DE, NL, FR, IT, ES, SE
+
+$config = json_decode('[{"label":"English (main language)","name":"tab1459950117904","hidden":false,"elements":[{"type":"section","name":"el1459950117904","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our <a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a> before starting. In doubt about which html-tags to use. Read our\u00a0<a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<\/p>"},{"type":"text","name":"el1459950117905","required":false,"label":"Title","value":"\u200b","microcopy":"Insert title of article here","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950117906","required":false,"label":"Script","value":"\u200b","microcopy":"Insert javascript here. Leave blank, if it isnt needed. ","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950117907","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"Insert body of article here. Use source code(&lt;&gt;) for styling.","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950117908","required":false,"label":"Script","value":"","microcopy":"Insert javascript here. Leave blank, if it isnt needed. ","limit_type":"words","limit":0,"plain_text":true}]},{"label":"DK","name":"tab1459950128216","hidden":false,"elements":[{"type":"section","name":"el1459950128216","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<\/p>"},{"type":"text","name":"el1459950128217","required":false,"label":"Title","value":"\u200b","microcopy":"Insert title of article here","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950128218","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950128219","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950128220","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"DE","name":"tab1459950126687","hidden":false,"elements":[{"type":"section","name":"el1459950126687","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1459950126688","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950126689","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950126690","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950126691","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"NL","name":"tab1459950125210","hidden":false,"elements":[{"type":"section","name":"el1459950125210","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1459950125211","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950125212","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950125213","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950125214","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"FR","name":"tab1459950123861","hidden":false,"elements":[{"type":"section","name":"el1459950123861","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1459950123862","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950123863","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950123864","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950123865","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"IT","name":"tab1459950121870","hidden":false,"elements":[{"type":"section","name":"el1459950121870","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1459950121871","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950121872","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950121873","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950121874","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"ES","name":"tab1459950146431","hidden":false,"elements":[{"type":"section","name":"el1459950146431","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1459950146432","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950146433","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1459950146434","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1459950146435","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]},{"label":"SE","name":"tab1460705656275","hidden":false,"elements":[{"type":"section","name":"el1460705656275","title":"Guidelines","subtitle":"<p>First time writing for the Support Center? Take a look at our\u00a0<a href=\"https:\/\/docs.google.com\/a\/trustpilot.com\/document\/d\/1WOEupgRTM7ed26U928wDdzJn_68geiSZeJRcVMut93g\/edit?usp=sharing\" target=\"_blank\">guidelines<\/a>\u00a0before starting. In doubt about which html-tags to use. Read our <a href=\"https:\/\/docs.google.com\/document\/d\/1FtUuzwMFM3oaRWeMsEexgKvg89C5yS90ZAVMnnWRst0\/edit#\" target=\"_blank\">design guidelines<\/a>.<span class=\"redactor-invisible-space\">\u200b<\/span><\/p>"},{"type":"text","name":"el1460705656276","required":false,"label":"Title","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1460705656277","required":false,"label":"Script","value":"\u200b","microcopy":"","limit_type":"words","limit":0,"plain_text":true},{"type":"text","name":"el1460705656278","required":false,"label":"Body","value":"<p>\n    \u200b\n<\/p>","microcopy":"","limit_type":"words","limit":0,"plain_text":false},{"type":"text","name":"el1460705656279","required":false,"label":"Script","value":"","microcopy":"","limit_type":"words","limit":0,"plain_text":true}]}]');
+
+// Above config object is constructed as response of Fixed Template definition, and can be dynamic
+
+require 'vendor/autoload.php';
+
+use Zendesk\API\HttpClient as ZendeskAPI;
+use GuzzleHttp\Client;
+
+$subdomain = "trustpilot"; //Replace with your zendesk domain name
+$username  = "..."; //Zendesk User name
+$token     = "..."; // Zendesk API Token
+$user_id = "805465647";
+
+$client = new ZendeskAPI($subdomain, $username);
+$client->setAuth('basic', ['username' => $username, 'token' => $token]);
+
+// Setup API client
+$client = new Client([
+    'base_uri' => 'https://api.gathercontent.com',
+    'headers' => [
+        'Accept' => 'application/vnd.gathercontent.v0.5+json'
+    ],
+    'auth' => [
+        "...", //GatherContent user name
+        "..."  //GatherContent API Key
+    ]
+]);
+
+function cURL($url)
+{
+    $ch     =   curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_USERPWD, "<Zendesk-User-Name>/token:<Zendesk-API-Token>");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+
+    $response   =   curl_exec($ch);
+    $response   =   json_decode($response,true);
+    curl_close($ch);
+    return $response;
+}
+
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+
+function replace_between($str, $needle_start, $needle_end, $replacement) {
+    $pos = strpos($str, $needle_start);
+    $start = $pos === false ? 0 : $pos + strlen($needle_start);
+
+    $pos = strpos($str, $needle_end, $start);
+    $end = $start === false ? strlen($str) : $pos;
+
+    return substr_replace($str,$replacement,  $start, $end - $start);
+}
+
+$page = 1;
+$arrArticles = [];
+
+$responseJson = cURL("https://trustpilot.zendesk.com/api/v2/help_center/articles.json?per_page=100&draft=false&page=1");
+//var_dump($responseJson);
+
+$arrArticles = $responseJson['articles'];
+
+//echo 'Total articles in first fetch ' . count($arrArticles);
+
+while ( $responseJson['next_page'] ) {
+	//echo 'There is next page '.$responseJson['next_page'];
+	$page = $page + 1;
+	$responseJson = cURL("https://trustpilot.zendesk.com/api/v2/help_center/articles.json?per_page=100&draft=false&page=".$page);
+	//var_dump($responseJson);
+	//echo 'Total articles in page='. $page .' fetch ' . count($responseJson['articles']);
+	$arrArticles = array_merge($arrArticles, $responseJson['articles']);
+}
+
+echo 'Total articles after merges ' . count($arrArticles);
+
+foreach($arrArticles as $article) {
+
+    $articleID = $article['id'];
+    $articleName = $article['title'];
+    $gelements = [];
+    $gtab = [];
+    $translationJson = cURL( "https://trustpilot.zendesk.com/api/v2/help_center/articles/" . $article['id'] ."/translations.json?locale=en-us,da,de,es,fr,it,nl,sv" );
+
+	//echo '*****************' . $articleID;
+	//var_dump($translationJson['translations']);
+
+    foreach($translationJson['translations'] as $translation) {
+        $sectionID = $translation['id'];
+        $locale = $translation['locale'];
+        $lastUpdate = $translation['updated_at'];
+        $sectionTitle = $translation['title'];
+        $sectionContent = $translation['body'];
+
+		if(strlen(get_string_between($sectionContent, '<script>','</script>')) > 0) {
+			$scriptBody = '<script>' . get_string_between($sectionContent, '<script>','</script>') . '</script>';
+		} else {
+			$scriptBody = '';
+		}
+
+        $sectionContent = replace_between($sectionContent, '<script>','</script>', '');
+
+        //echo "--> Article " . $articleID . " being imported now ";
+
+        $sectionUL = $sectionTitle . $sectionID;
+
+        // Sequence is : English, DK, DE, NL, FR, IT, ES, SE
+
+        //echo $locale . "\\\\\\\\\\\\\\\\\\\ \n";
+
+        $link_article = "<a href='https://support.trustpilot.com/hc/" . $locale ."/articles/".$articleID. "' target='_blank'\>https://support.trustpilot.com/hc/" . $locale . "/articles/" . $articleID . "</a>";
+
+        if(strcmp($locale,"en-us") == 0 ) {
+        	$config[0]->elements[1]->microcopy = $link_article;
+			$config[0]->elements[1]->value = $sectionTitle;
+			$config[0]->elements[2]->value = $scriptBody;
+			$config[0]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"da") == 0 ) {
+        	$config[1]->elements[1]->microcopy = $link_article;
+ 			$config[1]->elements[1]->value = $sectionTitle;
+ 			$config[1]->elements[2]->value = $scriptBody;
+			$config[1]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"de") == 0 ) {
+        	$config[2]->elements[1]->microcopy = $link_article;
+ 			$config[2]->elements[1]->value = $sectionTitle;
+ 			$config[2]->elements[2]->value = $scriptBody;
+			$config[2]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"nl") == 0 ) {
+        	$config[3]->elements[1]->microcopy = $link_article;
+ 			$config[3]->elements[1]->value = $sectionTitle;
+ 			$config[3]->elements[2]->value = $scriptBody;
+			$config[3]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"fr") == 0 ) {
+        	$config[4]->elements[1]->microcopy = $link_article;
+ 			$config[4]->elements[1]->value = $sectionTitle;
+ 			$config[4]->elements[2]->value = $scriptBody;
+			$config[4]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"it") == 0 ) {
+        	$config[5]->elements[1]->microcopy = $link_article;
+ 			$config[5]->elements[1]->value = $sectionTitle;
+ 			$config[5]->elements[2]->value = $scriptBody;
+			$config[5]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"es") == 0 ) {
+        	$config[6]->elements[1]->microcopy = $link_article;
+ 			$config[6]->elements[1]->value = $sectionTitle;
+ 			$config[6]->elements[2]->value = $scriptBody;
+			$config[6]->elements[3]->value = $sectionContent;
+        } else if(strcmp($locale,"sv") == 0 ) {
+        	$config[7]->elements[1]->microcopy = $link_article;
+ 			$config[7]->elements[1]->value = $sectionTitle;
+ 			$config[7]->elements[2]->value = $scriptBody;
+			$config[7]->elements[3]->value = $sectionContent;
+        }
+
+    }
+        // Create new
+
+        $gatherResponse = $client->post('/items', [
+                'form_params' => [
+                    'project_id' => "71097",
+                    'name' => $articleName,
+                    'template_id' => "307435",
+                    'config' => base64_encode(json_encode($config))
+                ]
+        ]);
+
+
+    //echo $gatherResponse->getStatusCode() . '(' . $gatherResponse->getReasonPhrase() . ') for ' . $articleName . ' = '. $gatherResponse->getHeader('location')[0]."\n<br>\n";
+
+
+}
